6121d152a706673d43a4fd53fd5958b779f3ee5a
Salamander <rohitroy741760@gmail.com>
2021-07-09 12:14:44 +0530

    Added Patch API

--- src/index.js
+++ src/index.js
@@ -28,7 +28,25 @@ app.get('/events', (req,res) => {
         res.status(400).send(error);
     })
 });
-
+app.patch('/events/:id', async (req,res) => {
+    const allowedUpdates = ['name', 'city', 'company', 'date'];
+    const updates = Object.keys(req.body);
+    const valid = updates.every((update) => allowedUpdates.includes(update));
+    if(!valid) {
+        return res.status(400).send('Invalid Update');
+    }
+    try {
+        const event = await Event.findOne({ _id: req.params.id });
+        if(!event) {
+            res.status(404).send();
+        }
+        updates.forEach((update) => event[update] = req.body[update]);
+        await event.save();
+        res.send(event);
+    }catch(e) {
+        res.status(500).send();
+    }
+})
 app.listen(port, () => {
     console.log('Server is running on '+port);
 })
